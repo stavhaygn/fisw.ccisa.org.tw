97f759572c121686652f3486f1ffd8915e5b1744
stavhaygn <stavhaygnzhan@gmail.com>
2023-01-01 07:47:04 +0800

    Update to 2023's sign-up information

--- src/pages/sign-up.js
+++ src/pages/sign-up.js
@@ -23,24 +23,23 @@ const SignUpPage = () => (
       <StyledUl>
         <StyledList>
           (一)點選
-          <BlankA href="https://forms.gle/2eUPWu3JrbLMNKyq6">
+          <BlankA href="https://docs.google.com/forms/d/e/1FAIpQLSdyzpW7WMHshe7mW7jPdGst13QeLXPXYbbNKGevWdtilG5m1g/viewform">
             <Emphasis>「線上課程」</Emphasis>
           </BlankA>
           (課程網址於活動前三天公告-將以Google Meet線上課程方式進行)或
         </StyledList>
         <StyledList tw="md:pl-16">
-          <BlankA href="https://forms.gle/2eUPWu3JrbLMNKyq6">
+          <BlankA href="https://docs.google.com/forms/d/e/1FAIpQLSdyzpW7WMHshe7mW7jPdGst13QeLXPXYbbNKGevWdtilG5m1g/viewform">
             <Emphasis>「實體課程」</Emphasis>
           </BlankA>
-          地點為國立臺灣科技大學-綜合研究大樓一樓(RB-102
-          國際會議廳)，填寫個人資料表單。
+          地點為集思北科會議中心二樓 (貝塔廳)，填寫個人資料表單。
         </StyledList>
         <StyledList>
           (二)上傳其它有利於審查之資料。方可完成報名程序。
         </StyledList>
       </StyledUl>
       <p tw="text-base md:text-lg">
-        ※經報名資料審核程序，將於2022年1月13日(四)於活動網站，公告審查報名通過之活動錄取名單，即可參加活動。
+      ※經報名資料審核程序，將於2023年2月3日(五)公告於活動網站，公告審查報名通過之活動錄取名單，即可參加活動。
       </p>
     </Column>
     <Column>
@@ -50,20 +49,18 @@ const SignUpPage = () => (
           <tr>
             <StyledTh>報名截止</StyledTh>
             <StyledTd>
-              即日起至2022年1月10日(一)
+              即日起至2023年1月16日(一)
               <Emphasis tw="block text-secondary">
-                課程人數：線上80名、實體50名，額滿為止！
+                課程人數：線上50名學生 & 30名銀行相關領域專業人士、實體50名，額滿為止！
               </Emphasis>
               <Emphasis tw="inline-block mt-1 pt-1 border-t border-dashed border-gray-500 text-red-600">
-                ★線上報名人數已超額，暫時無開放線上名額，敬請見諒！
-                <br />
-                如欲參加研習營，敬請報名實體課程，謝謝！
+                ★研習證書：兩日實體課程確實簽到結業者，結訓即頒發結業證書；線上參與者，請恕無法提供結業證書 (線上課程網址：將於活動前3天公告)。
               </Emphasis>
             </StyledTd>
           </tr>
           <tr>
             <StyledTh>放榜名單</StyledTh>
-            <StyledTd>2022年1月13日(四)</StyledTd>
+            <StyledTd>2023年2月3日(五)</StyledTd>
           </tr>
         </tbody>
       </table>
@@ -81,9 +78,9 @@ const SignUpPage = () => (
         `}
       >
         <StyledList>
-          研習課程約690分鐘，完成研習課程者，將頒發結業研習證書。(線上參與者，會後補發研習證書)
+          研習課程約540分鐘，實體完成研習課程者，將頒發結業研習證書。
         </StyledList>
-        <StyledList>結業贈送「精美小禮」與「摸彩」活動</StyledList>
+        <StyledList>實體課程結業並確實兩日簽到者，可參與「摸彩」活動</StyledList>
         <StyledUl
           css={[
             tw`pl-4`,
@@ -93,14 +90,7 @@ const SignUpPage = () => (
           ]}
         >
           <StyledList>
-            實體50名同學，結訓即贈水壺/結業證書。(選擇實體上課者，主辦單位於2022年1月18日(二)、1月19(三)中午皆備有便當。)
-          </StyledList>
-          <StyledList>
-            線上80名同學，結訓即贈7-11禮劵(NT100)/結業證書。
-          </StyledList>
-          <StyledList>
-            活動結束前，將於現場抽出3名幸運兒，送出價值$ 6,090 air pods乙台
-            (含實體及線上報名名單)。
+            活動結束前，將於現場抽出6名幸運兒，送出價值$7,490 AirPods Pro(第二代)乙台、價值$3,000 HomePod mini 三台、價值$990 AirTag 2個
           </StyledList>
         </StyledUl>
       </StyledUl>
