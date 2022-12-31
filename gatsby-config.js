b4f64b8201f5c35321e893f8867e171902ce87b2
stavhaygn <stavhaygnzhan@gmail.com>
2022-12-31 22:42:30 +0800

    Update information of workshop

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: '2022 金融資安研習營',
+    title: '2023 金融資安研習營',
     description:
-      '2022 金融資安研習營 Financial Information & Security Workshop 為協助大學財金、資工、資管及資訊類相關背景學生對於金融產業的認識及瞭解資安之實務運用，藉由整合產業的力量，舉辦此研習營以協助學生能夠於在校期間認識金融資安相關之工作，同時建立產業與學界共同合作的機制。',
+      '2023 金融資安研習營 Financial Information & Security Workshop 為協助大學財金、資工、資管及資訊類相關背景學生對於金融產業的認識及瞭解資安的實務運用，藉由整合產業的力量，舉辦此研習營以協助學生能夠於在校期間認識金融資安的工作，同時建立產業與學界共同合作的機制。',
     siteUrl: 'https://fisw.ccisa.org.tw',
   },
   plugins: [
