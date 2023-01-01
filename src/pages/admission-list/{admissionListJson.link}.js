b52ce9728418b5c2d04bbf3786b2147f07022227
stavhaygn <stavhaygnzhan@gmail.com>
2023-01-01 08:49:20 +0800

    Delete 2022's admission list

--- src/pages/admission-list/{admissionListJson.link}.js
+++ src/pages/admission-list/{admissionListJson.link}.js
@@ -6,6 +6,7 @@ import tw, { css } from 'twin.macro';
 import Page from '../../components/layout/Page';
 import Column from '../../components/Column';
 import TableDetail from '../../components/TableDetail';
+import SubTitle from '../../components/SubTitle';
 
 const ListButtonGroup = ({ nodes = [] }) => (
   <div tw="inline-flex items-center w-full mt-2 mb-4 justify-between md:w-min md:mb-6">
@@ -51,28 +52,7 @@ const chunk = (arr, len) => {
 const AdmissionListPage = ({ data }) => (
   <Page title="課程錄取名單">
     <Column tw="items-center">
-      <ListButtonGroup {...data.allAdmissionListJson} />
-      <table>
-        <thead>
-          <tr>
-            <TableDetail tw="pb-4 border-0 text-xl md:text-2xl" th colSpan="5">
-              {`${data.admissionListJson.text} ${data.admissionListJson.participants.length} 人`}
-            </TableDetail>
-          </tr>
-        </thead>
-        <tbody>
-          {chunk(data.admissionListJson.participants, 5).map(names => (
-            <tr>
-              {names.map(name => (
-                <TableDetail tw="px-3 border-0 md:px-10">{name}</TableDetail>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <p tw="mt-6 text-base text-secondary whitespace-pre-line md:text-lg">
-        {data.admissionListJson.note}
-      </p>
+      <SubTitle>待2023年2月3日(五)公告</SubTitle>
     </Column>
   </Page>
 );
